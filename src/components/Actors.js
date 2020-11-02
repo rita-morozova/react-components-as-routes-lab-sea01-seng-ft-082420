395b087ff0bd813555635c61383b804ab10a2fab
Margarita Morozova <ritamorozova@yahoo.com>
2020-11-02 11:28:06 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,19 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+     <h1>Actors Page</h1>
+     {actors.map((actor,index) => (
+          <div key={index}>
+            <h2>Name: {actor.name}</h2>
+             <h2>Movies: </h2>
+            <ul>
+              {actor.movies.map((movie, index) => (
+              <li key={index}>
+              {movie}
+              </li>
+              ))}</ul>
+          </div>
+     ))}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => (
+        <div key={movie.title}>
+          <h2>Title: {movie.title}</h2>
+          <h2>Time: {movie.time}</h2>
+          <h2>Genres: </h2>
+          <ul>
+            {movie.genres.map((genre, index) =>(
+              <li key={index}>
+                {genre}
+              </li>
+            ))}
+          </ul>
+          </div>))}
     </div>
   );
 };
